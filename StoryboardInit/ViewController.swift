615175edccb07346495730d9f89140bf25f03186
Ziloliddin <Zilolidddinkamolov@gmail.com>
2023-02-04 18:50:42 +0500

    Initial Commit

---
+++ StoryboardInit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardInit
+//
+//  Created by Ziloliddin Kamolov on 04/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
